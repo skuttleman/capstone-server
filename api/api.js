85abd2fc8cd0b4e5be71971935d390249d48df04
Ben Allred <skuttleman@gmail.com>
2016-03-15 08:27:06 -0600

    initial api layed out

---
+++ api/api.js
@@ -0,0 +1,6 @@
+var route = require('express').Router();
+
+module.exports = route;
+
+route.use('/v1/players', require('./v1/players'));
+// route.use('/v1/games', require('./v1/players'));
---
+++ api/v1/players.js
@@ -0,0 +1,25 @@
+
+route.get('/:id', function(request, response, next) {
+  knex('players').where({ id: request.params.id }).then(function(players) {
+    response.json({ players: players });
+  }).catch(next);
+});
+
+route.get('/', function(request, response, next) {
+  Promise.all([
+    knex('players'),
+    // knex('players').select('players.*', 'games.player1_id', 'games.player2_id', 'games.last_updated')
+    // .innerJoin('games', 'players.id', 'player1_id')
+    // .where('player1_id', request.user.id).orWhere('player2_id', request.user.id)
+  ]).then(function(results) {
+    var players = results[0];
+    // var online = sockets.userList();
+    // players.forEach(function(player) {
+    //   player.online = !!online.find((each) => each.id == player.id);
+    //   player.encountered = (results[1].find(function(each) {
+    //     return each.player1_id == player.id || each.player2_id == player.id;
+    //   }) || {}).last_updated;
+    // });
+    response.json({ players: players });
+  }).catch(next);
+});
