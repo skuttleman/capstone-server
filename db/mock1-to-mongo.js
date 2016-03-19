393263afce4208577c11690fb18f4f4fc645d883
Ben Allred <skuttleman@gmail.com>
2016-03-19 16:24:31 -0600

    updated srcipt to update mongo for both mock levels

--- db/mock1-to-mongo.js
+++ db/mock1-to-mongo.js
@@ -1,7 +1,22 @@
 var mongo = require('./mongo');
-var mock1 = require('../api/v1/mock1').data;
-var mock2 = require('../api/v1/mock2').data;
+var mocks = [
+  require('../api/v1/mock1').data,
+  require('../api/v1/mock2').data
+];
 
-mongo.runQuery('game_levels', 'update', {}, { $set: mock1 }).then(console.log);
+mongo.openDB().then(function(db) {
+  return mongo.withOpen(db, 'game_levels', 'find', {}).then(function(levels) {
+    return Promise.all(
+      mocks.map(updateLevel(db, levels))
+    );
+  }).then(function() {
+    db.close();
+  }).catch(console.error);
+});
 
-mongo.runQuery('game_levels', 'insert', mock2).then(console.log);
+function updateLevel(db, levels) {
+  return function(mock, i) {
+    return mongo.withOpen(db, 'game_levels', 'update', { _id: levels[i]._id },
+    { $set: mock });
+  };
+}
